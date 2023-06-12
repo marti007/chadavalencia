9a435aec3f97b301e6d96fa1379ec3886719d429
marti007 <66077632+marti007@users.noreply.github.com>
2023-06-12 18:17:16 +0800

    Add files via upload

--- valscripts.js
+++ valscripts.js
@@ -1,14 +1,6 @@
-const form = document.getElementById('my_form');
 
-form.addEventListener('submit', function handleSubmit(event) {
-  event.preventDefault();
 
-  // 👇️ Send data to the server here
-
-  // 👇️ Reset the form here
-  form.reset();
-});
-
+// SCROLL EFFECT
 function reveal() {
   var reveals = document.querySelectorAll(".reveal");
 
@@ -26,3 +18,58 @@ function reveal() {
 }
 
 window.addEventListener("scroll", reveal);
+
+// DATE AND TIME
+const displayTime = document.querySelector(".display-time");
+// Time
+function showTime() {
+  let time = new Date();
+  displayTime.innerText = time.toLocaleTimeString("en-US", { hour12: true });
+  setTimeout(showTime, 1000);
+}
+
+showTime();
+
+// Date
+function updateDate() {
+  let today = new Date();
+
+  // return number
+  let dayName = today.getDay(),
+    dayNum = today.getDate(),
+    month = today.getMonth(),
+    year = today.getFullYear();
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const dayWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  // value -> ID of the html element
+  const IDCollection = ["day", "daynum", "month", "year"];
+  // return value array with number as a index
+  const val = [dayWeek[dayName], dayNum, months[month], year];
+  for (let i = 0; i < IDCollection.length; i++) {
+    document.getElementById(IDCollection[i]).firstChild.nodeValue = val[i];
+  }
+}
+
+updateDate();
